78f6577f60e98de4f28d06ef469eb2f24617ca82
Brenda Franco and Pablo Barreyro <brenda@>
2018-03-19 10:33:13 -0700

    Set up essential project structure.

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var add = function(number1, number2) {
+	return number1 + number2;
+};
+
+var inputNumber1 = parseInt(prompt("Enter number: "));
+var inputNumber2 = parseInt(prompt("Enter another number: "));
+
+alert(add(inputNumber1,inputNumber2));
