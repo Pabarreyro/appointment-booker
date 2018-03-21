2e0d2ec0c121181ec5a89f1922387627ca553484
pb and ss <pb@>
2018-03-21 11:13:51 -0700

    Add style and script links to index.html; Set up scripts.js with document(ready) handler.

--- js/script.js
+++ js/script.js
@@ -1,33 +1,40 @@
-// var add = function(number1, number2) {
-// 	return number1 + number2;
-// };
-//
-// var subtract = function(number1, number2) {
-// 	return number1 - number2;
-// };
-//
-// var multiply = function(number1, number2) {
-// 	return number1 * number2;
-// };
-//
-// var divide = function(number1, number2) {
-// 	return number1 / number2;
-// };
+$(document).ready(function(event) {
+  var add = function(number1, number2) {
+	return number1 + number2;
+  };
+
+  var subtract = function(number1, number2) {
+	return number1 - number2;
+  };
+
+  var multiply = function(number1, number2) {
+  	return number1 * number2;
+  };
+
+  var divide = function(number1, number2) {
+  	return number1 / number2;
+  };
+
+  event.preventDefault();
+});
+
+
+
 
 // var bmi = function(massLb, heightIn) {
 //   return (massLb / heightIn**2) * 703;
 // };
-
-var inputTempCelsius = parseInt(prompt("Enter the temperature in celsius: "));
-var inputTempFahrenheit = parseInt(prompt("Enter the temperature in fahrenheit: "));
-
-var celsiusToFahrenheit = function(tempCelsius) {
-  return ((9/5)*tempCelsius) + 32;
-};
-
-var fahrenheitToCelsius = function(tempFahrenheit) {
-  return (tempFahrenheit - 32)*(5/9);
-};
-
-alert("The result is " + celsiusToFahrenheit(inputTempCelsius).toFixed(1) + " degrees fahrenheit");
-alert("The result is " + fahrenheitToCelsius(inputTempFahrenheit).toFixed(1) + " degrees celsius");
+//
+// var inputTempCelsius = parseInt(prompt("Enter the temperature in celsius: "));
+// var inputTempFahrenheit = parseInt(prompt("Enter the temperature in fahrenheit: "));
+//
+// var celsiusToFahrenheit = function(tempCelsius) {
+//   return ((9/5)*tempCelsius) + 32;
+// };
+//
+// var fahrenheitToCelsius = function(tempFahrenheit) {
+//   return (tempFahrenheit - 32)*(5/9);
+// };
+//
+// alert("The result is " + celsiusToFahrenheit(inputTempCelsius).toFixed(1) + " degrees fahrenheit");
+// alert("The result is " + fahrenheitToCelsius(inputTempFahrenheit).toFixed(1) + " degrees celsius");
