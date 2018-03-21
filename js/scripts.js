0e8f34919bd502e12f5d524a479f38c80e6866c1
pb and ss <pb@>
2018-03-21 11:55:44 -0700

    Add form functionality to index.html.

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,28 @@
-$(document).ready(function(event) {
-  var add = function(number1, number2) {
-	return number1 + number2;
-  };
+var add = function(number1, number2) {
+return number1 + number2;
+};
 
-  var subtract = function(number1, number2) {
-	return number1 - number2;
-  };
+var subtract = function(number1, number2) {
+return number1 - number2;
+};
 
-  var multiply = function(number1, number2) {
-  	return number1 * number2;
-  };
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
 
-  var divide = function(number1, number2) {
-  	return number1 / number2;
-  };
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
 
-  event.preventDefault();
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result= add(number1,number2);
+    $("#output p").text(result);
+    event.preventDefault();
+  })
+
 });
 
 
